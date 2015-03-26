763927fe684869232b9c1bc9e0427ae1714ed607
stasiekz <zielonks@ee.pw.edu.pl>
2015-03-26 18:36:33 +0100

    wprowadzono minimalna generacje statystki do pliku

--- src/statgen.c
+++ src/statgen.c
@@ -9,7 +9,13 @@
 
 void gen_stat(storage_t storage, param_t p, stat_t s) {
 
-	printf("\n\n\nLICZBA WYRAZOW W PLIKU WEJSCIOWYM %d \n", s.n_words_in);
-	printf("\n\n\nLICZBA WEZLOW W DRZEWIE %d \n\n\n", storage.v->n_nodes );
+	if(p.gen_stat) {
+
+		fprintf(p.stat_file, "\n\n\nLICZBA WYRAZOW W PLIKU WEJSCIOWYM %d \n", s.n_words_in);
+		fprintf(p.stat_file, "\n\n\nLICZBA WEZLOW W DRZEWIE %d \n\n\n", storage.v->n_nodes );
+
+		fclose(p.stat_file);
+	}
+
 
 }
