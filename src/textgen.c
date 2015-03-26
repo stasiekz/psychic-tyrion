54fc0bc18248527db26bbb9fee4720a59961b411
stasiekz <zielonks@ee.pw.edu.pl>
2015-03-26 11:39:10 +0100

    poprawiono modul textgen.c

--- src/textgen.c
+++ src/textgen.c
@@ -2,11 +2,22 @@
 #include "argparser.h"
 #include "statgen.h"
 #include "textgen.h"
+#include "reader.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+
+void add_buf( char **buf, char **pref, char *suf, int ngram ){
+	int i;
+	for (i = 1; i < ngram-1; i++){
+		buf[i-1] = pref[i];
+	}
+	buf[i-1] = suf;
+}
+
+
 
 void gen_text(storage_t storage, param_t p, stat_t *s) {
 
@@ -16,63 +27,52 @@ void gen_text(storage_t storage, param_t p, stat_t *s) {
 
 	srand( time(NULL) );
 
-	int n = p.n_gram; // zmienna do liczenia liczby slow wydrukowanych
-	int i, j, r;
+	int n = 0; // zmienna do liczenia liczby slow wydrukowanych
+	int i;
 	int prefix_size = p.n_gram == 1 ? 1 : p.n_gram-1;
+	char **buf = malloc (prefix_size*sizeof*buf);
+	char *suffix;
 	node_t *node = NULL;
-	char **buf = malloc (p.n_gram*sizeof*buf);
-
-
-	fprintf(p.output, "\t");
-	// unigram 
-	if( p.n_gram == 1 ) { // losuj prefixy z drzewa i drukuj do konca
-		while ( n < p.n_words ) {
-			r = rand() % storage.v->n_nodes; // losuj indeks dla wezla
-			fprintf(p.output, "%s ", storage.v->n[r]->d->prefix[0]); // drukuj pierwszy prefix
-			n++;
-		}
-	} else {
-
-		r = rand() % storage.v->n_nodes; // losuj indeks dla wezla 
-		for(i = 0; i < prefix_size; i++)
-			fprintf(p.output, "%s ", storage.v->n[r]->d->prefix[i]); // drukuj pierwszy prefix
-
-		j = rand() % storage.v->n[r]->d->n_suff; // losuj pierwszy suffix
-		fprintf(p.output, "%s ", storage.v->n[r]->d->suffix[j]);
 
 
 
-		for(i = 1; i < prefix_size; i++) // kopiuj prefix od poczatku bufora
-			buf[i-1] = storage.v->n[r]->d->prefix[i];
-		buf[prefix_size] = storage.v->n[r]->d->suffix[j]; // kopiuj suffix na przed ostatnia
+	while( n < p.n_words ) {
 
-		while( n < p.n_words ) {
+		if( !node ) {
+			node = storage.v->n[ rand()%storage.v->n_nodes ]; // losuj wezel drzewa
 
-			node = lookup_tree(storage.tree, buf, p.n_gram);
+			if( p.n_gram == 1) { // unigram
 
-			if( node == NULL ) {
-				r = rand() % storage.v->n_nodes; // losuj indeks dla wezla
-				j = rand() % storage.v->n[r]->d->n_suff;
-				fprintf(p.output, "%s ", storage.v->n[r]->d->suffix[j]);
+				fprintf(p.output, "%s ", node->d->prefix[0]); // wypisz prefix
+				node = NULL;
+				n++;
+				continue;
 
-
-				for(i = 1; i < prefix_size; i++) // kopiuj prefix od poczatku bufora
-					buf[i-1] = storage.v->n[r]->d->prefix[i];
-				buf[prefix_size] = storage.v->n[r]->d->suffix[j]; // kopiuj suffix na przed ostatnia
 			} else {
-				j = rand() % node->d->n_suff;
-				fprintf(p.output, "%s ", node->d->suffix[j]);
-
-
-				for(i = 1; i < prefix_size; i++) // kopiuj prefix od poczatku bufora
-					buf[i-1] = storage.v->n[r]->d->prefix[i];
-				buf[prefix_size] = storage.v->n[r]->d->suffix[j]; // kopiuj suffix na przed ostatnia
+				for( i = 0; i < prefix_size; i++)
+					fprintf(p.output, "%s ", node->d->prefix[i]); // wypisz prefix
+				fprintf(p.output, "%s ", suffix = node->d->suffix[ rand()%node->d->n_suff ] ); // losuj i wypisz suffix
+				n += p.n_gram;
+				add_buf(buf, node->d->prefix, suffix, p.n_gram); // dodaj dane do bufora
 			}
 
+		} else {
+
+			fprintf(p.output, "%s ", suffix = node->d->suffix[ rand()%node->d->n_suff ] ); // losuj i wypisz suffix
+			add_buf(buf, node->d->prefix, suffix, p.n_gram);
 			n++;
+
+
 		}
+
+		node = lookup_tree(storage.tree, buf, p.n_gram);
 	}
 
+
+
 	free(buf);
+
+
+
 }
 
