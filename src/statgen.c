c015c6e71be5f29a547302d71cb27206e3eec1b1
stasiekz <zielonks@ee.pw.edu.pl>
2015-03-31 23:24:40 +0200

    dodano statystyki

--- src/statgen.c
+++ src/statgen.c
@@ -12,7 +12,8 @@ void gen_stat(storage_t storage, param_t p, stat_t s) {
 	if(p.gen_stat) {
 
 		fprintf(p.stat_file, "\n\n\nLICZBA WYRAZOW W PLIKU WEJSCIOWYM %d \n", s.n_words_in);
-		fprintf(p.stat_file, "\n\n\nLICZBA WEZLOW W DRZEWIE %d \n\n\n", storage.v->n_nodes );
+		fprintf(p.stat_file, "\n\n\nLICZBA PREFIXOW W PLIKU WEJSCIOWYM %d \n\n\n", storage.v->n_nodes );
+		fprintf(p.stat_file, "\n\n\nLICZBA %d-gramow W PLIKU WEJSCIOWYM %d \n\n\n", p.n_gram, s.n_words_in-(p.n_gram-1) );
 
 		fclose(p.stat_file);
 	}
